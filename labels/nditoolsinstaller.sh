a98416f0f4e1e56ebb860fa9580e4e077c736711
the5isoutloud <past_pension_0n@icloud.com>
2025-08-06 10:03:21 -0500

    ndi-tools

--- labels/nditoolsinstaller.sh
+++ labels/nditoolsinstaller.sh
@@ -1,5 +1,8 @@
 nditoolsinstaller)
+    name="NDITools"
     type="pkg"
+    packageID="com.newtek.Application-Mac-NDI-ScanConverter"
     downloadURL="https://downloads.ndi.tv/Tools/NDIToolsInstaller.pkg"
+    appNewVersion=""
     expectedTeamID="W8U66ET244"
     ;;
