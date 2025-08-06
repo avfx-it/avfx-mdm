11398111734e04550ec065149728994512292be3
the5isoutloud <past_pension_0n@icloud.com>
2025-08-06 09:59:48 -0500

    nditools

--- labels/nditoolsinstaller.sh
+++ labels/nditoolsinstaller.sh
@@ -1,7 +1,5 @@
 nditoolsinstaller)
-    name="NDIToolsInstaller"
     type="pkg"
     downloadURL="https://downloads.ndi.tv/Tools/NDIToolsInstaller.pkg"
-    appNewVersion=""
     expectedTeamID="W8U66ET244"
     ;;
